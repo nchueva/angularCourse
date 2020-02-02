4aa9e20d1f55711262343a98ea98d8eaf5397173
Nadezhda Chueva <Nadezhda@linux.fritz.box>
2020-02-02 20:23:40 +0100

    remove widget-main-module

--- src/app/widget-main/widget-main.component.ts
+++ src/app/widget-main/widget-main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
 })
 
 
-export class WidgetMainComponent {
+export class WidgetMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
