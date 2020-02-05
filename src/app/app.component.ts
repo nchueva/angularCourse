cd1c35c90b76d6a47ca8ecdb8694ac86cd84864a
Nadezhda Chueva <Nadezhda@linux.fritz.box>
2020-02-05 01:28:22 +0100

    add hotels, add click event WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,68 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularCourse';
+  public title = 'angularCourse';
 
 constructor() { }
 
-  public getHotels() {
-    const hotels = [
-      {
-        img: 'burg-eltz.jpg',
-        address: 'Germany, Neustrasse 56, Burg, 40299',
-        phone: 133556658, // дополнительно задание pipe для форматирования
-        weather: {
-          title: 'Rain with sun',
-          icon: 'fa-cloud-sun',
-          water: 19,
-          temperature: 20,
-        },
-        social_info: {
-          title: 'Burg Hotel',
-          img: 'cap-de-formentor.jpg',
-          followers: 5000,
-          following: 100
-        },
-      },
-      {
-        img: 'tajikistan.jpg',
-        address: 'Germany, Somestreet 98, Augsburg, 60589',
-        phone: 2548785157, // дополнительно задание pipe для форматирования
-        weather: {
-          title: 'Sunny',
-          icon: 'fa-sun',
-          water: 10,
-          temperature: 15,
-        },
-        social_info: {
-          title: 'Augsburg Hotel',
-          img: 'winter.jpg',
-          followers: 1259,
-          following: 5000
-        },
-      },
-    ];
-
-    return hotels;
-
-  }
 }
 
-export interface Hotels {
-  img: string;
-  address: string;
-  phone: number; // дополнительно задание pipe для форматирования
-  weather: {
-    title: string,
-    icon: string,
-    water: number,
-    temperature: number
-  };
-  social_info: {
-    title: string,
-    img: string,
-    followers: number,
-    following: number
-  };
-  type: string;
-}
--- src/app/widget-main/widget-main.component.ts
+++ src/app/widget-main/widget-main.component.ts
@@ -8,11 +8,80 @@ import { Component, OnInit } from '@angular/core';
 })
 
 
-export class WidgetMainComponent implements OnInit {
+export class WidgetMainComponent {
+
+  public hotels = [
+    {
+      img: 'burg-eltz.jpg',
+      address: 'Germany, Neustrasse 56, Burg, 40299',
+      phone: 133556658, // дополнительно задание pipe для форматирования
+      weather: {
+        title: 'Rain with sun',
+        icon: 'fa-cloud-sun',
+        water: 19,
+        temperature: 20,
+      },
+      social_info: {
+        title: 'Burg Hotel',
+        img: 'cap-de-formentor.jpg',
+        followers: 5000,
+        following: 100
+      },
+    },
+    {
+      img: 'tajikistan.jpg',
+      address: 'Germany, Somestreet 98, Augsburg, 60589',
+      phone: 2548785157, // дополнительно задание pipe для форматирования
+      weather: {
+        title: 'Sunny',
+        icon: 'fa-sun',
+        water: 10,
+        temperature: 15,
+      },
+      social_info: {
+        title: 'Augsburg Hotel',
+        img: 'winter.jpg',
+        followers: 1259,
+        following: 5000
+      },
+    },
+  ];
+
+  public tempUrl = './assets/images/';
+  public firstImg: string = './assets/images/' + this.hotels[0].img;
+  public secondImageUrlOutput: string;
 
   constructor() { }
 
-  ngOnInit() {
+  public changeImgs(firstImgUrl: string, secondImgUrl: string) {
+    this.firstImg = this.tempUrl + firstImgUrl;
+
+    this.secondImageUrlOutput = this.tempUrl + secondImgUrl;
+
+    return;
   }
 
+
 }
+
+
+export interface Hotel {
+  img: string;
+  address: string;
+  phone: number; // дополнительно задание pipe для форматирования
+  weather: {
+    title: string,
+    icon: string,
+    water: number,
+    temperature: number
+  };
+  social_info: {
+    title: string,
+    img: string,
+    followers: number,
+    following: number
+  };
+  type: string;
+}
+
+interface Hotels extends Array<Hotel> { }
